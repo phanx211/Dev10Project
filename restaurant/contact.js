0fec6c2b90241c9fca2601299e08598a5b42658b
phanx211 <33839067+phanx211@users.noreply.github.com>
2018-10-25 23:26:15 -0500

    Added some comments

--- restaurant/contact.js
+++ restaurant/contact.js
@@ -1,7 +1,10 @@
 function validateForm() {
+  // Retrieve the 3 required form fields
   var x = document.forms["contactForm"]["name"].value;
   var y = document.forms["contactForm"]["email"].value;
   var z = document.forms["contactForm"]["phone"].value;
+
+  // Check to make sure they're not all empty
   if (x != "") {
     if (y != "") {
       if (z != "") {
